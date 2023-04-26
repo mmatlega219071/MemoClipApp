767c525b7cef589e4785f91e6162c13d45a40d14
Karol Kozioł <79792386+s193470@users.noreply.github.com>
2023-04-26 14:56:28 +0200

    index.js update

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Register from '../components/Register.vue';
 import Home from '../components/Home.vue';
 import VideoList from '../components/VideoList.vue';
 import RecordVideo from '../components/RecordVideo.vue';
 import AppSettings from '../components/AppSettings.vue';
 import LogIn from '../components/LogIn.vue';
-import Register from '../components/Register.vue';
+import WelcomeSite from '../components/Welcome.vue';
+
 
 const routes = [
+  { path: '/welcome', component: WelcomeSite },
   { path: '/', component: Home },
   { path: '/video-list', component: VideoList },
   { path: '/record-video', component: RecordVideo },
