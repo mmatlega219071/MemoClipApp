3bb5613c8cc29a0c3879b6f397803011b4931a0d
Mateusz Matlega <mateusz.matlega@ivn.pl>
2023-05-02 21:13:00 +0200

    notifications functionality

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,39 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cron = require("node-cron");
+admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Ustawienia harmonogramu powiadomień
+const notificationTimes = ["0 8 * * *", "0 22 * * *"];
+
+// Treść powiadomienia
+const payload = {
+  notification: {
+    title: "Nagrywanie wideo",
+    body: "Nie zapomnij nagrać swojego dzisiejszego wideo!",
+  },
+};
+
+// Funkcja wysyłająca powiadomienia do wszystkich użytkowników
+async function sendNotificationsToAllUsers() {
+  const usersSnap = await admin.firestore().collection("users").get();
+  for (const userDoc of usersSnap.docs) {
+    const user = userDoc.data();
+    if (user.fcmToken) {
+      await admin.messaging().sendToDevice(user.fcmToken, payload);
+    }
+  }
+}
+
+// Rejestracja harmonogramu powiadomień
+notificationTimes.forEach((time) => {
+  cron.schedule(time, () => {
+    sendNotificationsToAllUsers();
+  });
+});
+
+// Eksport funkcji, jeśli chciałbyś ją wywołać ręcznie
+exports.sendNotifications = functions.https.onRequest(async (req, res) => {
+  await sendNotificationsToAllUsers();
+  res.send("Powiadomienia wysłane");
+});
