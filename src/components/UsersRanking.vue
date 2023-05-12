2d24ee050b531ba1285b4ddf006b84014b95ece9
Kamila Cikowska <cikowska.k@gmail.com>
2023-05-12 11:41:48 +0200

    fixes ranking

--- src/components/UsersRanking.vue
+++ src/components/UsersRanking.vue
@@ -1,50 +1,51 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <router-link to="/" class="navbar-brand"
-          ><img
-            src="../../public/img/icons/AppIcon.png"
-            alt="AppIcon"
-            class="logo"
-        /></router-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link to="/video-list" class="nav-link"
-                >Video list</router-link
-              >
-            </li>
-            <li class="nav-item">
-              <router-link to="/app-settings" class="nav-link"
-                >Credits</router-link
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div class="container">
-      <div>
-        <h3>Top 5 users</h3>
-        <div class="box">
-          <p v-for="(topUser, index) in topFiveUsers" :key="index">
-            {{ topUser.userFirstName }} {{ topUser.userLastName }} | Number of video: {{ topUser.liczbaWideo }}
-          </p>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand"
+        ><img
+          src="../../public/img/icons/AppIcon.png"
+          alt="AppIcon"
+          class="logo"
+      /></router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/video-list" class="nav-link"
+              >Video list</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/app-settings" class="nav-link"
+              >Credits</router-link
+            >
+          </li>
+        </ul>
       </div>
     </div>
-  </template>
+  </nav>
+  <div class="container">
+    <div>
+      <h3>Top 5 users</h3>
+      <div class="box">
+        <p v-for="(topUser, index) in topFiveUsers" :key="index">
+          {{ topUser.userFirstName }} {{ topUser.userLastName }} | Number of
+          video: {{ topUser.videoNumber }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 import { getAllVideos } from "../lib/memoClipApiClient";
@@ -73,7 +74,8 @@ table {
   width: 100%;
 }
 
-td, th {
+td,
+th {
   border: 1px solid #fff;
   text-align: left;
   padding: 8px;
